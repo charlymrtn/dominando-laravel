2a0f049a8091994e6d27ba81a398795d3903bcb5
Carlos Martín <cjmar87@gmail.com>
2019-02-06 16:27:53 -0600

    terminamos con etiquetas

--- basic/resources/views/tags/show.blade.php
+++ basic/resources/views/tags/show.blade.php
@@ -1,32 +1,47 @@
 @extends('layout')
 
-@section('title','Zonas')
+@section('title','Etiquetas')
 
 @section('contenido')
-    @if($zona)
-        <h1>Zona S1-{{$zona->id}}</h1>
-        <h2>{{$zona->entidad_tipo}}</h2>
-        <h3>{{$zona->zonable_id}}</h3>
-        <p>{{$zona->body}}</p>
+    @if($tag)
+        <h1>Etiqueta T1-{{$tag->id}}</h1>
+        <h2>{{$tag->name}}</h2>
+
         <br>
-        <p>{{get_class($zona->entidad)}}</p>
-        <h4>{{$zona->entidad_tipo}} de la zona S1-{{$zona->id}}</h4>
-        <table class="table">
-            <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Llave</th>
-                <th scope="col">Nombre</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-                <th scope="row">{{$zona->entidad->id}}</th>
-                <td>{{$zona->entidad->name}}</td>
-                <td>{{$zona->entidad->key}}</td>
-            </tr>
-            </tbody>
-        </table>
+
+        <div class="row">
+            <div class="col-md-6">
+                <p>
+                <h3>Usuarios de la etiqueta</h3>
+                @foreach($tag->usuarios as $user)
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$user->id}}</h5>
+                            <p class="card-text">{{$user->name}}</p>
+                            <p class="card-text">{{$user->email}}</p>
+                            <a href="{{route('usuarios.show',$user->id)}}" class="btn btn-primary">Ver Usuario</a>
+                        </div>
+                    </div>
+                    @endforeach
+                    </p>
+            </div>
+            <div class="col-md-6">
+                <p>
+                <h3>Mensajes de la etiqueta</h3>
+                @foreach($tag->mensajes as $mensaje)
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$mensaje->id}}</h5>
+                            <p class="card-text">{{$mensaje->name}}</p>
+                            <p class="card-text">{{$mensaje->email}}</p>
+                            <p class="card-text">{{$mensaje->mensaje}}</p>
+                            <a href="{{route('mensajes.show',$mensaje->id)}}" class="btn btn-primary">Ver Mensaje</a>
+                        </div>
+                    </div>
+                    @endforeach
+                    </p>
+            </div>
+        </div>
 
     @else
         <h3>no existe este elemento</h3>
